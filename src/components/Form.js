62eff1ad16f3faca00099aece32283bb84698323
RaheemaMuhammed <121420610+RaheemaMuhammed@users.noreply.github.com>
2023-06-22 12:26:31 +0530

    added validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,55 @@
- 
-import React from 'react'
 
+
+import React,{useState} from 'react'
+import './Form.css'
 function Form(props) {
-    
+
+    const [inputError,setInputError]= useState('')
     const inputTextHandler=(e)=>{
         console.log(e.target.value);
         props.setInputText(e.target.value)
+        setInputError('')
     }
     const submitTodoHandler=(e)=>{
         e.preventDefault();
+        const realInput=props.inputText.trim();
+        if(realInput.trim()===''){
+            setInputError("Enter valid todo");
+            return
+        }
+        const todoExists=props.todos.some((todo)=>todo.text.toLowerCase() === realInput.toLowerCase());
+        if(todoExists){
+            setInputError("ToDo Already Exists")
+            return
+        }
         props.setTodos([
-            ...props.todos,{text:props.inputText, completed:false , id:Math.random()*1000}
+            ...props.todos,{text:realInput, completed:false , id:Math.random()*1000}
         ]);
         props.setInputText("")
     }
+  
+   
   return (
+    <div>
+    {inputError?<p className="error-msg">{inputError}</p>:null}
        <form >
+      
       <input
        value={props.inputText} 
        onChange={inputTextHandler} 
        type="text" 
        className="todo-input"
        required />
+       
+      
+
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
-    </form>  ) 
+    </form>  
+    </div>
+    ) 
 }
+
 
 export default Form
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Todo({text,setTodos, todo,todos}) {
+    
     const deleteHandler=()=>{
        setTodos(todos.filter((el)=> el.id !== todo.id ));
     }
@@ -14,11 +15,17 @@ function Todo({text,setTodos, todo,todos}) {
             return item;
         }))
     }
+    const editHandler=()=>{
+
+    }
   return (
     <div className="todo">
         <li className={`todo-item ${todo.completed ? "completed":''}`}>{text}</li>
         <button onClick={completeHandler} className='complete-btn'>
             <i className='fas fa-check'></i>
+        </button>
+        <button onClick={editHandler} className='edit-btn'>
+            <i className='fas fa-edit'></i>
         </button>
         <button onClick={deleteHandler} className='trash-btn'>
             <i className='fas fa-trash'></i>
